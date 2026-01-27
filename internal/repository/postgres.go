9e0b97122254a72f93e85800535ea5c6262d6860
Lolopenza <lolopenza@gmail.com>
2026-01-27 18:20:27 +0500

    postgres connection

---
+++ internal/repository/postgres.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"em_tz/internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB(cfg *config.DatabaseConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", cfg.DSN())
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to postgres: %w", err)
+	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	return db, nil
+}
