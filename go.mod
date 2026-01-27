859d75eb866123aff947f7ef1e6e910ef4e25bc9
Lolopenza <lolopenza@gmail.com>
2026-01-27 15:29:14 +0500

    model definition and migrations

---
+++ go.mod
@@ -0,0 +1,9 @@
+module em_tz
+
+go 1.25.4
+
+require (
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ internal/models/subscriptions.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"time"
+
+	uuid "github.com/jackc/pgx/pgtype/ext/gofrs-uuid"
+)
+
+type Subscription struct {
+	ID          uuid.UUID  `json:"id" db:"id"`
+	ServiceName string     `json:"service_name" db:"service_name"`
+	Price       int        `json:"price" db:"price"`
+	UserID      uuid.UUID  `json:"user_id" db:"user_id"`
+	StartDate   time.Time  `json:"start_date" db:"start_date"`
+	EndDate     *time.Time `json:"end_date,omitempty" db:"end_date"`
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at" db:"updated_at"`
+}
+
+type CreateSubmissionReq struct {
+	ServiceName string `json:"service_name" validate:"required"`
+	Price       int    `json:"price" validate:"required,min=1"`
+	UserID      string `json:"user_id" validate:"required,uuid"`
+	StartDate   string `json:"start_date" validate:"required"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+type UpdateSubmissionReq struct {
+	ServiceName string `json:"service_name,omitempty"`
+	Price       int    `json:"price,omitempty" validate:"omitempty,min=1"`
+	StartDate   string `json:"start_date,omitempty"`
+	EndDate     string `json:"end_date,omitempty"`
+}
